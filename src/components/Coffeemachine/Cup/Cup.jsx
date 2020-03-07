315583dc03486d496ff0184b2a34234640614a88
Dennis Voltz <dennis.voltz@wwu.de>
2020-03-07 23:34:12 +0100

    Add ready cup

--- src/components/Coffeemachine/Cup/Cup.jsx
+++ src/components/Coffeemachine/Cup/Cup.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./Cup.css";
 
 export default function Cup({ id, quantity, type, active, fillLevel, status }) {
+  console.log(active);
+  const style = {
+    backgroundImage: 'url("cup_ready.svg")'
+  };
+
   return (
-    <div className="cup">
+    <div className="cup" style={active ? style : {}}>
       {/* <p>{quantity > 0 ? quantity : "empty"}</p> */}
       {type !== null ? <img src={type + ".svg"} /> : ""}
       {/* <p>{fillLevel}</p>
-      <p>{status}</p>
-      <p>{active ? "!active!" : ""}</p> */}
+      <p>{status}</p> */}
     </div>
   );
 }
